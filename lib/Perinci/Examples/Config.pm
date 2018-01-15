e46e49201c8a2beece4398ecf4039dc6b043e97a
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2018-01-15 20:53:39 +0700

    Release v0.001

--- lib/Perinci/Examples/Config.pm
+++ lib/Perinci/Examples/Config.pm
@@ -19,22 +19,30 @@ $SPEC{config_info} = {
     v => 1.1,
     summary => 'Show config file information',
     args => {
+        arg_int1  => {schema=>'int*'},
+        arg_int2  => {schema=>'int*'},
+        arg_str1  => {schema=>'str*'},
+        arg_str2  => {schema=>'str*'},
+        arg_hash1 => {schema=>'hash*'},
     },
 };
 sub config_info {
     my %args = @_;
 
-    my $r = $args{-cmdline_r}
+    my $cmdline = delete($args{-cmdline});
+
+    my $r = delete($args{-cmdline_r})
         or return [412, "I am not passed -cmdline_r, please set ".
                    "Perinci::CmdLine's attribute: pass_cmdline_object"];
 
     [200, "OK", {
+        args              => \%args,
         read_config       => $r->{read_config},
         read_config_files => $r->{read_config_files},
         config            => $r->{config},
         config_paths      => $r->{config_paths},
         config_profile    => $r->{config_profile},
-    ];
+    }];
 }
 
 1;
